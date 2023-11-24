91baabf15650d7e6b2a55a2890f3d95395630b62
Lucas Alves <lucasalves3.0@hotmail.com>
2023-11-24 16:02:20 -0300

    Listrando produto unico

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,7 +1,7 @@
 @extends('site.layout')
 @section('title', 'Essa é a página HOME')
-
 @section('conteudo')
+
 <div class="row container">
 
     @foreach ($produtos as $produto)
@@ -11,7 +11,7 @@
                 <div class="card-image">
                 <img src="{{ $produto->imagem }}">
                 <span class="card-title">{{ $produto->nome }}</span>
-                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
+                <a href="{{ route('site.details', $produto->slug )}}" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
                 </div>
                 <div class="card-content">
                 <p> {{ Str::limit($produto->descricao, 20) }} </p>
--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,7 @@ use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('produtos', ProdutoController::class);
+
 Route::get('/', [SiteController::class,'index'])->name('site.index');
+
+Route::get('/produto/{slug}', [SiteController::class, 'details'])->name('site.details');
