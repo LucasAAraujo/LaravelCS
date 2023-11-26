df934de71492918b6f3475facdbaac797b4ad4ee
Lucas Alves <lucasalves3.0@hotmail.com>
2023-11-26 11:50:21 -0300

    Criando carrinho e adicionando itens

--- resources/views/site/details.blade.php
+++ resources/views/site/details.blade.php
@@ -2,17 +2,27 @@
 @section('title', 'Detalhes')
 @section('conteudo')
 
-<div class="row container">
+<div class="row container"><br>
     <div class="col s12 m6">
         <img src="{{ $produto->imagem }}" class="responsive-img">   
     </div>
     <div class="col s12 m6">
         
-        <h1> {{ $produto->nome }}</h1>  
+        <h4> {{ $produto->nome }}</h4>  
+        <h4> R$ {{ number_format($produto->preco, 2, ',','.' )}}</h4>  
         <p> Postado por: {{ $produto->user->Firstname }} <br>
             Categoria: {{ $produto->Categoria->nome}}
         </p>
+
+        <form action="{{ route('site.addCarrinho')}}" method="POST" enctype="multipart/form-data">    
+            @csrf
+            <input type="hidden" name="id" value="{{ $produto->id }}">
+            <input type="hidden" name="name" value="{{ $produto->nome }}">
+            <input type="hidden" name="price" value="{{ $produto->preco }}">
+            <input type="number" name="qnt" value="1">
+            <input type="hidden" name="img" value="{{ $produto->imagem }}">
         <button class="btn orange btn-large"> Comprar </button>
+        </form>
     </div>
 </div>
 
