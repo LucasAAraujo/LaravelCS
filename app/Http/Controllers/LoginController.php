a0d4b5222a8b81d1c8c63dbd26bd34427620cb3c
Lucas Alves <lucasalves3.0@hotmail.com>
2023-11-27 08:54:15 -0300

    Autenticação de usuário e criação de formulário

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,10 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    
+    public function auth(Request $request) {
+
+        $credenciais = $request->validate([
+            'email' => 'required', 'email',
+            'password'=> 'required',
+        ],[
+            'email.required'=> 'O campo email é obriatório!',
+            'wmail.email'=> 'O e-mail não é válido',
+            'password.required'=> 'O campo senha é obrigatório!'
+            ]);
+        
+        if(Auth::attempt($credenciais)) {
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard');
+        } else {
+            return redirect()->back()->with('erro', 'Usuário ou senha inválida.');
+        }
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,21 @@
 <?php
 
 use App\Http\Controllers\CarrinhoController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('produtos', ProdutoController::class);
-
 Route::get('/', [SiteController::class,'index'])->name('site.index');
-
 Route::get('/produto/{slug}', [SiteController::class, 'details'])->name('site.details');
-
 Route::get('/categoria/{id}', [SiteController::class, 'categoria'])->name('site.categoria');
 
 Route::get('/carrinho', [CarrinhoController::class, 'carrinhoLista'])->name('site.carrinho');
-
 Route::post('/carrinho', [CarrinhoController::class, 'adicionaCarrinho'])->name('site.addCarrinho');
-
 Route::post('/remover', [CarrinhoController::class, 'removeCarrinho'])->name('site.removeCarrinho');
-
 Route::post('/atualizar', [CarrinhoController::class, 'atualizaCarrinho'])->name('site.atualizaCarrinho');
-
 Route::get('/limpar', [CarrinhoController::class, 'limparCarrinho'])->name('site.limparCarrinho');
+
+Route::view('/login', 'login.form')->name('login.form');
+Route::post('/auth', [LoginController::class,'auth'])->name('login.auth');
