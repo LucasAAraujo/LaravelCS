34a5f4d192e67cea5c2fd0ac0e6c7fc28fd5ce10
Lucas Alves <lucasalves3.0@hotmail.com>
2023-11-27 08:27:17 -0300

    Finalizando carrinho e e ajustes

--- resources/views/site/carrinho.blade.php
+++ resources/views/site/carrinho.blade.php
@@ -4,28 +4,101 @@
 
 <div class="row container">
 
-    <h5>Categoria: {{ $categoria->nome }}</h5>
+    @if ($mensagem = Session::get('sucesso'))
 
-    @foreach ($produtos as $produto)
+    <div class="card green darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">Parabéns!</span>
+          <p>{{ $mensagem }}</p>
+        </div>
+    </div>
         
-        <div class="col s12 m4">
-            <div class="card">
-                <div class="card-image">
-                <img src="{{ $produto->imagem }}">
-                <span class="card-title">{{ $produto->nome }}</span>
-                <a href="{{ route('site.details', $produto->slug )}}" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
-                </div>
-                <div class="card-content">
-                <p> {{ Str::limit($produto->descricao, 20) }} </p>
-                </div>
+    @endif
+
+    @if ($mensagem = Session::get('aviso'))
+
+    <div class="card blue darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">Clear!</span>
+          <p>{{ $mensagem }}</p>
+        </div>
+    </div>
+        
+    @endif
+
+    @if ($itens->count() == 0)
+       
+        <div class="card orange darken-1">
+            <div class="card-content white-text">
+            <span class="card-title">Seu carrinho está vazio.</span>
+            <p>Compre algo!</p>
             </div>
         </div>
 
-    @endforeach
-</div>
+    @else
+    <h5>Seu carrinho possui {{ $itens->count() }} </h5>   
+        
+    <table class="striped">
+        <thead>
+        <tr>
+            <th></th>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Quantidade</th>
+            <th>Ações</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach ($itens as $item)
+            <tr>
+                <td><img src="{{$item->attributes->imagem}}" alt="" width="70px"></td>
+                <td>{{$item->name}}</td>
+                <td>R$ {{ number_format($item->price, 2, ',','.' )}}</td>
+                
+                {{--BTN ATUALIZAR--}}
+                <form action=" {{route('site.atualizaCarrinho')}}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <input type="hidden" name="id" value="{{ $item->id }}">
+                <td><input style="width: 40px, font-weight:900" class="white center" min="1" type="number" name="quantity" value="{{$item->attributes->quantity}}">
+                <td>
+                    <button class="btn-floating waves-effect waves-light orange"><i class="material-icons">refresh</i></button>
+                </form>
 
-<div class="row center">
-    {{ $produtos->links('custom.pagination') }}
+                {{--BTN REMOVER--}}
+                    <form action="{{ route('site.removeCarrinho')}}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <input type="hidden" name="id" value="{{ $item->id }}">
+                    <button class="btn-floating waves-effect waves-light red"><i class="material-icons">delete</i></button>    
+                    </form>
+                </td>    
+                
+            </tr>
+            @endforeach
+        </tbody>
+    
+    </table>
+
+    <h5>Valor total: </h5>
+
+    <div class="card orange darken-1">
+        <div class="card-content white-text">
+        <span class="card-title">R$ {{ number_format(\Cart::getTotal(), 2, ',','.' )}}</span>
+        <p>Pague em 4x sem juros!</p>
+        </div>
+    </div>
+
+    @endif
+   
+
+    <div class="row container center">
+        <a href="{{ route('site.index')}}" class="btn waves-effect waves-light blue">Continuar contando<i class="material-icons">arrow_back</i></a>
+        <a href="{{ route('site.limparCarrinho') }}" class="btn waves-effect waves-light blue">Limpar carrinho<i class="material-icons">clear</i></a>
+        <button class="btn waves-effect waves-light green">Finalizar carrinho<i class="material-icons">check</i></button>
+    </div>
+
+
+
 </div>
 
 
--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -22,8 +22,8 @@
           <a href="#" class="brand-logo center">Curso Laravel</a>
           <ul id="nav-mobile" class="left">
             <li><a href="{{ route('site.index') }}">Home</a></li>
-            <li><a href="" class="dropdown-trigger btn" data-target='dropdown1'"> Categorias <i class="material-icons right">expand_more</i></a></li>
-            <li><a href="badges.html">carrinho</a></li>
+            <li><a href="" class="dropdown-trigger btn red" data-target='dropdown1'"> Categorias <i class="material-icons right">expand_more</i></a></li>
+            <li><a href="{{ route('site.carrinho') }}">Carrinho <span class="new badge blue" data-badge-caption="">{{\Cart::getContent()->count()}}</span></a></li>
           </ul>
         </div>
     </nav>
